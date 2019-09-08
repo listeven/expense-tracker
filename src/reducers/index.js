95241c2084fe90b0a750072df28920b5601c1f70
Steven Li <listeven@bu.edu>
2019-09-08 17:54:11 -0400

    Added backend and connected to frontend

--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-import transactions from './transactions'
-
-// Can easily add more reducers later on
-const expenseTracker = combineReducers({
-    transactions
-})
-
-export default expenseTracker
